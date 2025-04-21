ce36f0fb116cd72f031fddd798b539d6b2e10a06
savitskiy1995 <savitskiy@raexpert.ru>
2025-04-21 12:23:47 +0300

    создание групп

---
+++ test/test_add_group.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create_group(Group(name="anyname", footer="anyfooter"))
+    app.session.logout()
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create_group(Group(name="", header="", footer=""))
+    app.session.logout()
+
