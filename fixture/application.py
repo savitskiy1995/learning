b9bb6f63b74721719ef7ac0305427b65323413ae
savitskiy1995 <savitskiy@raexpert.ru>
2025-04-21 10:46:24 +0300

    выделен вспомогательный класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,17 +1,22 @@
 from selenium.webdriver.firefox.webdraiver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 class Application:
 
     def __init__(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = WebDriver()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-    def test_untitled_test_case(self):
+        self.group = GroupHelper(self)
+    def open_home_page(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.create_group(wd)
-
+        wd.get("https://localhost/addressbook")
 
     def destroy(self):
         self.wd.quit()
+
+
+
+
+
+
