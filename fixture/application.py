5618f8f3c94f0d755fc83830e3c99fa059e5949c
savitskiy1995 <savitskiy@raexpert.ru>
2025-04-21 10:08:21 +0300

    выделен вспомогательный класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,28 +1,17 @@
 from selenium.webdriver.firefox.webdraiver import WebDriver
-
+from fixture.session import SessionHelper
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome(executable_path=r'')
         self.wd.implicitly_wait(30)
-
+        self.session = SessionHelper(self)
     def test_untitled_test_case(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd)
         self.create_group(wd)
 
-    def login(self, wd):
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def destroy(self):
         self.wd.quit()
--- test/addressbook_test.py
+++ test/addressbook_test.py
@@ -5,20 +5,17 @@ import pytest
 
 @pytest.fixture
 def app(request):
-    fixture = Applicatiom()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-  class test_add_group(unittest.Testcase):
-    def create_group(self, wd):
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("testname")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_link_text("Logout").click()
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.create_group(Group(name="dfgdfg", footer="dfgdfgfhq"))
+    app.session.logout()
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.session.logout()
 
