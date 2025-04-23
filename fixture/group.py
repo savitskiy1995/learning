6a691de6c7dfb8febd7523f7a31551807097cc43
savitskiy1995 <savitskiy@raexpert.ru>
2025-04-23 16:50:28 +0300

    Условия для запуска тестов с модификацией групп

--- fixture/group.py
+++ fixture/group.py
@@ -19,14 +19,7 @@ class GroupHelper:
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -64,12 +57,6 @@ class GroupHelper:
         self.change_field_value("group.name", group.name)
         self.change_field_value("group.header", group.header)
         self.change_field_value("group.footer", group.footer)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("group.header")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("group.footer")
 
     def change_field_value(self):
         if not group.name is not None:
@@ -96,3 +83,13 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_element_by_name("selected[]"))
+
+    def count_name_symbols(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        return len(wd.find_element_by_name("group_name"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,10 +1,22 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    if app.group.count_name_symbols == 0:
     app.group.modify_first_group(Group(name="New group"))
+
+def test_modify_group_name(app):
+    if app.group.count_name_symbols != 0:
+    app.group.modify_first_group(Group(name=None))
+
+
+
+
+
 
 #def test_modify_group_header(app):
 #   app.group.modify_first_group(Group(name="New header"))
 
 #def test_modify_group_footer(app):
 #    app.group.modify_first_group(Group(name="New footer"))
+
+
